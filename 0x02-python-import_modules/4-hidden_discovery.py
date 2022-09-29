bf627720bd03c716022e2785987b417f01befef1
Yelo94 <akpamib@yahoo.com>
2022-09-29 03:17:17 +0000

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import hidden_4
-def main():
-    for i in dir(hidden_4):
-        if i[0:2] != "__":
-            print(i)
-if __name__ == "__main__":
-    main()
+import sys
+import hidden_4 as hidden
+
+if __name__ != "__main__":
+    exit()
+
+for name in dir(hidden):
+    if name[0:2] != "__":
+        print(name)
