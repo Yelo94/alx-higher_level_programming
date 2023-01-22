9513aedd3e5df9d25b5e9fa5331312d256e9619a
Yelo94 <akpamib@yahoo.com>
2023-01-22 20:33:10 +0000

    100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import requests as r
+from sys import argv
+if __name__ == "__main__":
+    j = r.get(
+        'https://swapi.co/api/people/', params={'search': argv[1]}).json()
+    print("Number of results: " + str(j['count']))
+    for result in j['results']:
+        print(result['name'])
+
+
